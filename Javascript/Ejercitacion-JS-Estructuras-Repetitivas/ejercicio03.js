264acca6fecda0cf9329ce19a20e48d4afbff558
Zunino <Zuninojuanbautista@gmail.com>
2024-07-07 18:28:58 -0300

    Ej js: Estructuras Repetitivas N.3

--- Javascript/Ejercitacion-JS-Estructuras-Repetitivas/ejercicio03.js
+++ Javascript/Ejercitacion-JS-Estructuras-Repetitivas/ejercicio03.js
@@ -1,2 +1,6 @@
 // Ejercicio 3
 // Imprimir un listado con los números del 1 al 100 cada uno con su respectivo cuadrado
+
+for (let i = 1; i <= 100; i++) {
+  console.log(`${i}, Cuadrado: ${i * i}`)
+}
