129c34100935dde2bff60c2374e2f5545436578b
Zunino <Zuninojuanbautista@gmail.com>
2024-07-07 20:50:36 -0300

    Ej js: Estructuras Repetitivas N.7

--- Javascript/Ejercitacion-JS-Estructuras-Repetitivas/ejercicio07.js
+++ Javascript/Ejercitacion-JS-Estructuras-Repetitivas/ejercicio07.js
@@ -1,2 +1,26 @@
 // Ejercicio 7
 // Realizar la operación de potenciación (a^b), de dos valores enteros positivos, con multiplicaciones.
+
+const prompt = require('prompt-sync')()
+
+const base = parseInt(prompt('Ingrese el número: '))
+const exponente = parseInt(prompt('Ingrese el exponente: '))
+
+console.log(`Número: ${base}, exponente: ${exponente}.`)
+
+if (isNaN(base) || isNaN(exponente)) {
+  console.log('ERROR: Tipo de dato no válido.')
+} else {
+  let resultado = 1
+  if (exponente < 0) {
+    console.log('ERROR: El exponente debe ser un número entero positivo')
+  } else if (exponente > 0) {
+    for (let contador = 1; contador <= exponente; contador++) {
+      resultado *= base
+    }
+  }
+
+  // prettier-ignore
+  console.log(
+    `El resultado de la potencia ${base}^${exponente} es: ${resultado}.`)
+}
