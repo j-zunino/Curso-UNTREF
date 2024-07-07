cc27a0d1f2fc36142a06cb8e0d145d47c1556281
Zunino <Zuninojuanbautista@gmail.com>
2024-07-07 18:49:48 -0300

    Ej js: Estructuras Repetitivas N.4

--- Javascript/Ejercitacion-JS-Estructuras-Repetitivas/ejercicio04.js
+++ Javascript/Ejercitacion-JS-Estructuras-Repetitivas/ejercicio04.js
@@ -1,3 +1,13 @@
 // Ejercicio 4
 // Imprimir un listado con los números impares desde 1 hasta 999 y seguidamente otro listado con los
 // números pares desde 2 hasta 1000
+
+console.log('Números impar')
+for (let i = 1; i < 1000; i += 2) {
+  console.log(i)
+}
+
+console.log('Números par')
+for (let i = 2; i < 1000; i += 2) {
+  console.log(i)
+}
