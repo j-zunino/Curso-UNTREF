eecb73e4cb372f0e197c7a0fdfa89a1b164fc2b7
Zunino <Zuninojuanbautista@gmail.com>
2024-07-07 23:22:43 -0300

    Ej js: Estructuras Repetitivas N.10

--- Javascript/Ejercitacion-JS-Estructuras-Repetitivas/ejercicio10.js
+++ Javascript/Ejercitacion-JS-Estructuras-Repetitivas/ejercicio10.js
@@ -1,3 +1,13 @@
 // Ejercicio 10
 // Imprimir los 100 primeros números de Fibonacci. Recuerde que un número de Fibonacci se calcula
 // como la suma de los dos anteriores así: 0, 1, 1, 2, 3, 5, 8, 13...
+
+let a = 0
+let b = 1
+
+for (let i = 0; i <= 100; i++) {
+  console.log(`Número ${i}, ${a}`)
+  const numero = a
+  a = b
+  b = numero + b
+}
