c83e6b55535ed5f726fd165888f95d361480ee2d
Zunino <Zuninojuanbautista@gmail.com>
2024-07-10 20:37:07 -0300

    Ej js: Estructuras Repetitivas N.15

--- Javascript/Ejercitacion-JS-Estructuras-Repetitivas/ejercicio15.js
+++ Javascript/Ejercitacion-JS-Estructuras-Repetitivas/ejercicio15.js
@@ -4,3 +4,25 @@
 // • No múltiplo de 5.
 // • Mayor que 100.
 // • Menor que 10.000.
+
+do {
+  const prompt = require('prompt-sync')()
+
+  const numeroInput = parseInt(prompt('Ingrese un número: '))
+
+  if (isNaN(numeroInput)) {
+    console.log('ERROR: Dato ingresado no valido.')
+  } else {
+    if (
+      numeroInput % 2 === 0 &&
+      numeroInput % 5 !== 0 &&
+      numeroInput > 100 &&
+      numeroInput < 10000
+    ) {
+      console.log(`El número ${numeroInput} cumple con todas las condiciones.`)
+      break
+    } else {
+      console.log(`El número ${numeroInput} no cumple con las condiciones.`)
+    }
+  }
+} while (true)
