f81716196d85cb7ad8f61a6a4817a3ce99baaee3
Zunino <Zuninojuanbautista@gmail.com>
2024-08-08 15:04:06 -0300

    Ej js: funciones N.3

--- Javascript/Ejercitacion-JS-Funciones-/ejercicio03.js
+++ Javascript/Ejercitacion-JS-Funciones-/ejercicio03.js
@@ -1,3 +1,11 @@
 // Función F(X, Y):
 
 // Haga un programa en JavaScript que llame a la función F(X, Y) = 5.6 * X + Y / 2.5 y que muestre los resultados para F(3, 5); F(9, 2) y F(56, 87).
+
+const F = (X, Y) => {
+  return console.log((5.6 * X + Y / 2.5).toFixed(2));
+};
+
+F(3, 5);
+F(9, 2);
+F(56, 87);
