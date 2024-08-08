365bb9253793ece4b909c6a203d8eeb27ff08970
Zunino <Zuninojuanbautista@gmail.com>
2024-08-08 14:54:14 -0300

    Ej js: funciones N.2

--- Javascript/Ejercitacion-JS-Funciones-/ejercicio01.js
+++ Javascript/Ejercitacion-JS-Funciones-/ejercicio01.js
@@ -2,14 +2,14 @@
 
 // Escriba un programa en JavaScript con una función que tome como parámetros dos enteros, los sume y devuelva como resultado la suma. El programa deberá pedir al usuario los dos números, luego llamar a la función con los números leídos como argumentos y, por último, informar al usuario el valor de la suma.
 
-const prompt = require('prompt-sync')()
+const prompt = require('prompt-sync')();
 
 const suma = (numA, numB) => {
-  console.log(`Números a sumar: ${numA}, ${numB}`)
-  console.log(`El resultado de la suma es: ${numA + numB}`)
-}
+  console.log(`Números a sumar: ${numA}, ${numB}`);
+  console.log(`El resultado de la suma es: ${numA + numB}`);
+};
 
-const numA = parseInt(prompt('Ingrese el primer número a sumar: '))
-const numB = parseInt(prompt('Ingrese el segundo número a sumar: '))
+const numA = parseInt(prompt('Ingrese el primer número a sumar: '));
+const numB = parseInt(prompt('Ingrese el segundo número a sumar: '));
 
-suma(numA, numB)
+suma(numA, numB);
--- Javascript/Ejercitacion-JS-Funciones-/ejercicio02.js
+++ Javascript/Ejercitacion-JS-Funciones-/ejercicio02.js
@@ -1,3 +1,18 @@
 // Suma con tercer argumento:
 
 // Escriba un programa en JavaScript similar al anterior, pero esta vez utilizando una función que tome un tercer argumento y coloque el resultado de la suma en dicho parámetro.
+
+const prompt = require('prompt-sync')();
+
+const suma = (numA, numB, numC) => {
+  numC = numA + numB;
+  return numC;
+};
+
+const numA = parseInt(prompt('Ingrese el primer número a sumar: '));
+const numB = parseInt(prompt('Ingrese el segundo número a sumar: '));
+const numC = 0;
+
+const resultado = suma(numA, numB, numC);
+console.log(`Números a sumar: ${numA}, ${numB}`);
+console.log(`El resultado de la suma es: ${resultado}`);
