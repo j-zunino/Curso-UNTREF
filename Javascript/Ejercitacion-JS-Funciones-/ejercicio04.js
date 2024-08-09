fe5bf24c7a91caee1cdb1fc992f8a29e4640ac99
Zunino <Zuninojuanbautista@gmail.com>
2024-08-09 18:07:25 -0300

    Ej js: funciones N.4

--- Javascript/Ejercitacion-JS-Funciones-/ejercicio04.js
+++ Javascript/Ejercitacion-JS-Funciones-/ejercicio04.js
@@ -1,3 +1,22 @@
 // Conversión de minúscula a mayúscula:
 
 // Escriba un programa en JavaScript que utilice una función que reciba un carácter como parámetro, si el carácter es una letra minúscula, modifique el parámetro para convertirlo en mayúscula.
+
+const prompt = require('prompt-sync')();
+
+const checkCase = (letra) => {
+  if (letra === !letra.toLowerCase()) {
+    return console.log('Letra mayúscula.');
+  } else {
+    return console.log(
+      `Letra minúscula, convertida a mayúscula: ${letra.toUpperCase()}`
+    );
+  }
+};
+
+let input = '';
+
+do {
+  input = prompt('Ingrese una letra: ');
+  checkCase(input);
+} while (input.length !== 1);
