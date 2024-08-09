e614feca15a645ff01c47ca1f7fd10c31af64706
Zunino <Zuninojuanbautista@gmail.com>
2024-08-09 19:38:26 -0300

    Ej js: funciones N.5

--- Javascript/Ejercitacion-JS-Funciones-/ejercicio05.js
+++ Javascript/Ejercitacion-JS-Funciones-/ejercicio05.js
@@ -1,3 +1,20 @@
 // Factorial de un número:
 
 // Escriba un programa en JavaScript con una función que reciba un valor entero positivo y devuelva su factorial.
+
+const prompt = require('prompt-sync')();
+
+const factorial = (factorial) => {
+  let resultado = 1;
+  for (let i = 1; i <= factorial; i++) {
+    resultado *= i;
+  }
+  return console.log(`El factorial de ${factorial} es: ${resultado}`);
+};
+
+let num = 0;
+
+do {
+  num = parseInt(prompt('Ingrese un número positivo: '));
+  factorial(num);
+} while (num < 0);
