01521a7d4e702344beccf7c5275fe0483572c98e
j-zunino <zuninojuanbautista@gmail.com>
2024-10-03 22:21:19 -0300

    chore: reformat ej js arrays

--- Javascript/Ejercitacion-JS-Arrays/actividad2.js
+++ Javascript/Ejercitacion-JS-Arrays/actividad2.js
@@ -1,11 +1,11 @@
 // Actividad 2: Acceder y mostrar el tercer elemento del array de películas.
 
 const peliculas = [
-  "Joker",
-  "El rey leon",
-  "Toys Story",
-  "Blanca nieves",
-  "La bella y la bestia",
+    "Joker",
+    "El rey leon",
+    "Toys Story",
+    "Blanca nieves",
+    "La bella y la bestia",
 ];
 
 console.log(peliculas[2]);
